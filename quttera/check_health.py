2fcd8f3ceddfdf0d39f7c13376748cda3d148524
cs-dilkhush-kumar <kumard@fortinet.com>
2022-09-02 12:47:16 +0530

    Check Health updated

--- quttera/check_health.py
+++ quttera/check_health.py
@@ -1,12 +1,14 @@
 from .make_rest_api_call import MakeRestApiCall
+from .get_blacklist_status import get_blacklist_status
 
 
 def _check_health(config):
     try:
-        endpoint = ""
-        method = "GET"
-        MS = MakeRestApiCall(config=config)
-        MS.make_request(endpoint=endpoint, method=method)
+        params = {
+            "domain_name": "https://google.com",
+            "response_format": "Json"
+        }
+        resp = get_blacklist_status(config, params)
         return True
     except Exception as e:
         raise Exception(e)

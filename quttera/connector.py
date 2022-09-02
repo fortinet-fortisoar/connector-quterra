46d9b427684f4d0c9bfc75fa85bf28bb3b72c766
cs-dilkhush-kumar <kumard@fortinet.com>
2022-09-02 17:34:30 +0530

    Connector descriptions added

--- quttera/connector.py
+++ quttera/connector.py
@@ -24,24 +24,3 @@ class CustomConnector(Connector):
             return _check_health(config)
         except Exception as err:
             raise ConnectorError(err)
-    
-    def on_app_start(self, config, active):
-        pass
-
-    def on_add_config(self, config, active):
-        pass
-
-    def on_update_config(self, old_config, new_config, active):
-        pass
-
-    def on_delete_config(self, config):
-        pass
-
-    def on_activate(self, config):
-        pass
-
-    def on_deactivate(self, config):
-        pass
-
-    def teardown(self, config):
-        pass
